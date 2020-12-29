b290e53ac2224f8ea0b61ec6d74b47d37d267011
v_n_z list.ru <v_n_z@list.ru>
2020-12-29 09:46:18 +0300

    Добавьте файлы проекта.

---
+++ AsptCodeFirst/Migrations/20201228200416_SalaryAndDOF.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsptCodeFirst.Migrations
+{
+    public partial class SalaryAndDOF : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Salary",
+                table: "Employees",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DOF",
+                table: "Departments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salary",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "DOF",
+                table: "Departments");
+        }
+    }
+}
---
+++ AsptCodeFirst/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AsptCodeFirst
+{
+
+    class Department
+
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DOF { get; set; }
+
+    }
+    class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+    }
+    class EFOrganazationDB: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=Organization;Trusted_Connection=True;");
+        }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EFOrganazationDB efCntx = new EFOrganazationDB();
+            //List<Department> deps = efCntx.Departments.ToList();
+
+            //Department d = new Department()
+            //{
+            //    Description = "Development",
+            //    Name = "Dev"
+            //};
+
+            //efCntx.Departments.Add(d);
+            //efCntx.SaveChanges();
+        }
+    }
+}
